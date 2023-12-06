9f7ad4a40dc7a2b8aab2e4a3ddf417d1ddb13e8b
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-06 15:05:45 -0700

    Update ProductController.php

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -10,22 +10,133 @@ use App\Http\Requests\UpdateProductRequest;
 use App\Models\Product;
 use App\Models\ProductCategory;
 use App\Models\ProductTag;
+use App\Models\ProductType;
+use App\Models\TechnicalSpec;
 use Gate;
 use Illuminate\Http\Request;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Symfony\Component\HttpFoundation\Response;
+use Yajra\DataTables\Facades\DataTables;
 
 class ProductController extends Controller
 {
     use MediaUploadingTrait;
 
-    public function index()
+    public function index(Request $request)
     {
         abort_if(Gate::denies('product_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $products = Product::with(['categories', 'tags', 'media'])->get();
+        if ($request->ajax()) {
+            $query = Product::with(['categories', 'tags', 'technical_specs', 'product_type'])->select(sprintf('%s.*', (new Product)->table));
+            $table = Datatables::of($query);
 
-        return view('admin.products.index', compact('products'));
+            $table->addColumn('placeholder', '&nbsp;');
+            $table->addColumn('actions', '&nbsp;');
+
+            $table->editColumn('actions', function ($row) {
+                $viewGate      = 'product_show';
+                $editGate      = 'product_edit';
+                $deleteGate    = 'product_delete';
+                $crudRoutePart = 'products';
+
+                return view('partials.datatablesActions', compact(
+                    'viewGate',
+                    'editGate',
+                    'deleteGate',
+                    'crudRoutePart',
+                    'row'
+                ));
+            });
+
+            $table->editColumn('id', function ($row) {
+                return $row->id ? $row->id : '';
+            });
+            $table->editColumn('published', function ($row) {
+                return '<input type="checkbox" disabled ' . ($row->published ? 'checked' : null) . '>';
+            });
+            $table->editColumn('name', function ($row) {
+                return $row->name ? $row->name : '';
+            });
+            $table->editColumn('description', function ($row) {
+                return $row->description ? $row->description : '';
+            });
+            $table->editColumn('price', function ($row) {
+                return $row->price ? $row->price : '';
+            });
+            $table->editColumn('msrp', function ($row) {
+                return $row->msrp ? $row->msrp : '';
+            });
+            $table->editColumn('photo', function ($row) {
+                if ($photo = $row->photo) {
+                    return sprintf(
+                        '<a href="%s" target="_blank"><img src="%s" width="50px" height="50px"></a>',
+                        $photo->url,
+                        $photo->thumbnail
+                    );
+                }
+
+                return '';
+            });
+            $table->editColumn('additional_photos', function ($row) {
+                if (! $row->additional_photos) {
+                    return '';
+                }
+                $links = [];
+                foreach ($row->additional_photos as $media) {
+                    $links[] = '<a href="' . $media->getUrl() . '" target="_blank"><img src="' . $media->getUrl('thumb') . '" width="50px" height="50px"></a>';
+                }
+
+                return implode(' ', $links);
+            });
+            $table->editColumn('documents', function ($row) {
+                if (! $row->documents) {
+                    return '';
+                }
+                $links = [];
+                foreach ($row->documents as $media) {
+                    $links[] = '<a href="' . $media->getUrl() . '" target="_blank">' . trans('global.downloadFile') . '</a>';
+                }
+
+                return implode(', ', $links);
+            });
+            $table->editColumn('category', function ($row) {
+                $labels = [];
+                foreach ($row->categories as $category) {
+                    $labels[] = sprintf('<span class="label label-info label-many">%s</span>', $category->name);
+                }
+
+                return implode(' ', $labels);
+            });
+            $table->editColumn('tag', function ($row) {
+                $labels = [];
+                foreach ($row->tags as $tag) {
+                    $labels[] = sprintf('<span class="label label-info label-many">%s</span>', $tag->name);
+                }
+
+                return implode(' ', $labels);
+            });
+            $table->editColumn('technical_specs', function ($row) {
+                $labels = [];
+                foreach ($row->technical_specs as $technical_spec) {
+                    $labels[] = sprintf('<span class="label label-info label-many">%s</span>', $technical_spec->name);
+                }
+
+                return implode(' ', $labels);
+            });
+            $table->addColumn('product_type_name', function ($row) {
+                return $row->product_type ? $row->product_type->name : '';
+            });
+
+            $table->editColumn('slug', function ($row) {
+                return $row->slug ? $row->slug : '';
+            });
+
+            $table->rawColumns(['actions', 'placeholder', 'published', 'photo', 'additional_photos', 'documents', 'category', 'tag', 'technical_specs', 'product_type']);
+
+            return $table->make(true);
+        }
+
+        return view('admin.products.index');
     }
 
     public function create()
@@ -36,7 +147,11 @@ class ProductController extends Controller
 
         $tags = ProductTag::pluck('name', 'id');
 
-        return view('admin.products.create', compact('categories', 'tags'));
+        $technical_specs = TechnicalSpec::pluck('name', 'id');
+
+        $product_types = ProductType::pluck('name', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        return view('admin.products.create', compact('categories', 'product_types', 'tags', 'technical_specs'));
     }
 
     public function store(StoreProductRequest $request)
@@ -44,8 +159,17 @@ class ProductController extends Controller
         $product = Product::create($request->all());
         $product->categories()->sync($request->input('categories', []));
         $product->tags()->sync($request->input('tags', []));
+        $product->technical_specs()->sync($request->input('technical_specs', []));
         if ($request->input('photo', false)) {
             $product->addMedia(storage_path('tmp/uploads/' . basename($request->input('photo'))))->toMediaCollection('photo');
+        }
+
+        foreach ($request->input('additional_photos', []) as $file) {
+            $product->addMedia(storage_path('tmp/uploads/' . basename($file)))->toMediaCollection('additional_photos');
+        }
+
+        foreach ($request->input('documents', []) as $file) {
+            $product->addMedia(storage_path('tmp/uploads/' . basename($file)))->toMediaCollection('documents');
         }
 
         if ($media = $request->input('ck-media', false)) {
@@ -63,9 +187,13 @@ class ProductController extends Controller
 
         $tags = ProductTag::pluck('name', 'id');
 
-        $product->load('categories', 'tags');
+        $technical_specs = TechnicalSpec::pluck('name', 'id');
 
-        return view('admin.products.edit', compact('categories', 'product', 'tags'));
+        $product_types = ProductType::pluck('name', 'id')->prepend(trans('global.pleaseSelect'), '');
+
+        $product->load('categories', 'tags', 'technical_specs', 'product_type');
+
+        return view('admin.products.edit', compact('categories', 'product', 'product_types', 'tags', 'technical_specs'));
     }
 
     public function update(UpdateProductRequest $request, Product $product)
@@ -73,6 +201,7 @@ class ProductController extends Controller
         $product->update($request->all());
         $product->categories()->sync($request->input('categories', []));
         $product->tags()->sync($request->input('tags', []));
+        $product->technical_specs()->sync($request->input('technical_specs', []));
         if ($request->input('photo', false)) {
             if (! $product->photo || $request->input('photo') !== $product->photo->file_name) {
                 if ($product->photo) {
@@ -84,6 +213,34 @@ class ProductController extends Controller
             $product->photo->delete();
         }
 
+        if (count($product->additional_photos) > 0) {
+            foreach ($product->additional_photos as $media) {
+                if (! in_array($media->file_name, $request->input('additional_photos', []))) {
+                    $media->delete();
+                }
+            }
+        }
+        $media = $product->additional_photos->pluck('file_name')->toArray();
+        foreach ($request->input('additional_photos', []) as $file) {
+            if (count($media) === 0 || ! in_array($file, $media)) {
+                $product->addMedia(storage_path('tmp/uploads/' . basename($file)))->toMediaCollection('additional_photos');
+            }
+        }
+
+        if (count($product->documents) > 0) {
+            foreach ($product->documents as $media) {
+                if (! in_array($media->file_name, $request->input('documents', []))) {
+                    $media->delete();
+                }
+            }
+        }
+        $media = $product->documents->pluck('file_name')->toArray();
+        foreach ($request->input('documents', []) as $file) {
+            if (count($media) === 0 || ! in_array($file, $media)) {
+                $product->addMedia(storage_path('tmp/uploads/' . basename($file)))->toMediaCollection('documents');
+            }
+        }
+
         return redirect()->route('admin.products.index');
     }
 
@@ -91,7 +248,7 @@ class ProductController extends Controller
     {
         abort_if(Gate::denies('product_show'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        $product->load('categories', 'tags');
+        $product->load('categories', 'tags', 'technical_specs', 'product_type');
 
         return view('admin.products.show', compact('product'));
     }
