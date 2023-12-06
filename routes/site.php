620501b64f2967331f52a7d128367b4c2f7f1d13
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-06 12:55:25 -0700

    fixed products controller namespace

--- routes/site.php
+++ routes/site.php
@@ -1,6 +1,24 @@
 <?php
 use Illuminate\Support\Facades\Cache;
 
+// Route::get('/', function () {
+//     return view('site.home.index');
+// })->name('site.home');
+
+Route::get('/', 'SiteController@index')->name('homepage');
+Route::get('contact', 'SiteController@contact')->name('contact');
+Route::resource('blog', 'BlogController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+
+// // Route::get('load-more-data', 'BlogController@more_data');
+// // Route::get('load-more-blog', 'BlogController@more_press_blog')->name('load-more-blog');
+
+Route::resource('products', 'ProductsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
+
+
+
+
+
+
 Route::get('r', function () {
     function philsroutes()
     {
@@ -32,17 +50,3 @@ Route::get('r', function () {
 
     return philsroutes();
 })->name('assigned-routes');
-
-// Route::get('/', function () {
-//     return view('site.home.index');
-// })->name('site.home');
-
-Route::get('/', 'SiteController@index')->name('homepage');
-Route::get('contact', 'SiteController@contact')->name('contact');
-Route::resource('blog', 'BlogController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
-
-// // Route::get('load-more-data', 'BlogController@more_data');
-// // Route::get('load-more-blog', 'BlogController@more_press_blog')->name('load-more-blog');
-
-Route::resource('products', 'ProductsController', ['except' => ['create', 'store', 'edit', 'update', 'destroy']]);
-
