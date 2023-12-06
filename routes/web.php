fb0ad1f7cdb73733b1eee7a286d063abde38b4a8
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-06 14:22:06 -0700

    redirect adjustments for admin after login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/admin', function () {
 Auth::routes(['register' => false]);
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('admin.home');
     // Permissions
     Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
     Route::resource('permissions', 'PermissionsController');
