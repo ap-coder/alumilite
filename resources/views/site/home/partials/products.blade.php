b8214b5b7354bdbd885b8f1fc946b937e23f6a50
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-08 15:26:13 -0700

    Update products.blade.php

--- resources/views/site/home/partials/products.blade.php
+++ resources/views/site/home/partials/products.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <div class="section-title">
-                        <h2 class="title">Our Work</h2>
+                        <h2 class="title">MODELS</h2>
                     </div>
                 </div>
             </div>
