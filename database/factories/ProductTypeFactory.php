0e71f97bddfcbbc3010cc8b1f0b0c29b3eb5b6c2
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-08 20:45:08 -0700

    Create ProductTypeFactory.php

---
+++ database/factories/ProductTypeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductType>
+ */
+class ProductTypeFactory extends Factory
+{
+    protected $model = ProductType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'published' => $this->faker->boolean,
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug,
+            // 'created_at' and 'updated_at' will be automatically set by Eloquent
+        ];
+    }
+
+    // Add any additional methods or states you require here
+}
