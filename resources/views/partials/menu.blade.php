311549f0d126d0d23f99412a6f39362bc7d08d79
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-08 18:55:36 -0700

    Update menu.blade.php

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -276,9 +276,7 @@
         @endif
         <li class="c-sidebar-nav-item">
             <a href="#" class="c-sidebar-nav-link" onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
-                <i class="c-sidebar-nav-icon fas fa-fw fa-sign-out-alt">
-
-                </i>
+                <i class="c-sidebar-nav-icon fas fa-fw fa-sign-out-alt"></i>
                 {{ trans('global.logout') }}
             </a>
         </li>
