92b8cb9950e24ca0f9f693a4f95038656f72b22c
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-11 18:16:01 -0700

    Create pagination.blade.php

---
+++ resources/views/site/blog/partials/pagination.blade.php
@@ -0,0 +1,11 @@
+{{--                    <div class="all-pagination">--}}
+{{--                        <ul class="pagination ">--}}
+{{--                            <li><a class="previous" href="#"><i class="ion-ios-arrow-back"></i> <span>Previous</span></a></li>--}}
+{{--                            <li><a class="active" href="#">1</a></li>--}}
+{{--                            <li><a href="#">2</a></li>--}}
+{{--                            <li><a href="#">3</a></li>--}}
+{{--                            <li><a href="#">...</a></li>--}}
+{{--                            <li><a href="#">8</a></li>--}}
+{{--                            <li><a class="next" href="#"><span>Next</span> <i class="ion-ios-arrow-forward"></i></a></li>--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
