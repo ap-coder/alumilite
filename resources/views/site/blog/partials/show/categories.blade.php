4eae53e03f2db20b46713e3cdfd776a350490c3e
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 19:04:16 -0700

    Update categories.blade.php

--- resources/views/site/blog/partials/show/categories.blade.php
+++ resources/views/site/blog/partials/show/categories.blade.php
@@ -13,17 +13,4 @@
                                 </div>
                             </div>
 
-                            @if($blog_menu)
-                            <div class="blog-sidebar-category">
-                                <h3 class="sidebar-title">Menu</h3>
 
-                                <div class="category-list">
-                                    <ul class="list">
-                                        @foreach($blog_menu as $menu)
-                                        <li><a href="{{ preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode(strpos($menu['link'], "http") === 0 ? $menu['link'] : url('', $menu['link']))) }}">{{ $menu['label'] }}</a></li>
-                                        @enforeach
-
-                                    </ul>
-                                </div>
-                            </div>
-                            @endif
