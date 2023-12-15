363f7acb52b6a5fe0073a96cb6f7462c5b6b653e
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 19:04:11 -0700

    Create blog-menu.blade.php

---
+++ resources/views/site/blog/partials/show/blog-menu.blade.php
@@ -0,0 +1,13 @@
+@if($blog_menu)
+    <div class="blog-sidebar-category">
+        <h3 class="sidebar-title">Menu</h3>
+
+        <div class="category-list">
+            <ul class="list">
+                @foreach($blog_menu as $menu)
+                <li><a href="{{ preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode(strpos($menu['link'], "http") === 0 ? $menu['link'] : url('', $menu['link']))) }}">{{ $menu['label'] }}</a></li>
+                @enforeach
+            </ul>
+        </div>
+    </div>
+@endif
