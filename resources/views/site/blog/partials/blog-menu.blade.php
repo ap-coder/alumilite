f4f4a364f31ce5201500f7c7d6d2f52b2f854523
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 19:39:24 -0700

    Update blog-menu.blade.php

--- resources/views/site/blog/partials/blog-menu.blade.php
+++ resources/views/site/blog/partials/blog-menu.blade.php
@@ -5,7 +5,7 @@
             <ul class="list">
                 @foreach($blog_menu as $menu)
                 <li><a href="{{ preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode(strpos($menu['link'], "http") === 0 ? $menu['link'] : url('', $menu['link']))) }}">{{ $menu['label'] }}</a></li>
-                @enforeach
+                @endforeach
             </ul>
         </div>
     </div>
