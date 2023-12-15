2751b86c6963bd31d609cda8efd840589e6727a9
Tapas Vishwas <tapas_vishwas@bradycorp.com>
2023-12-16 01:41:47 +0530

    responsive fixes

--- resources/views/site/products/show/partials/product-images.blade.php
+++ resources/views/site/products/show/partials/product-images.blade.php
@@ -1,14 +1,14 @@
                         <div class="inventory-single-dealership-gallery">
                             <div class="dealership-gallery-active">
                                 @if($product->photo)
-                                    <img src="{{ $product->photo->getUrl() }}" alt="{{ $product->name }}">
+                                        {{ $product->getFirstMedia('photo')('responsive') }}
                                     @else
                                     <img src="{{ asset('assets/images/inventory-single-dealership/dealership-1.jpg') }}" alt="{{ $product->name }}">
                                 @endif
 
                                 @if($product->additional_photos->count()>0)
                                     @foreach ($product->additional_photos as $photos)
-                                        <img src="{{ $photos->getUrl() }}" alt="{{ $product->name }}">
+                                        {{ $photos('responsive') }}
                                     @endforeach
                                 @else
                                     <img src="{{ asset('assets/images/inventory-single-dealership/dealership-1.jpg') }}" alt="{{ $product->name }}">
@@ -22,7 +22,7 @@
                                 @if($product->photo)
                                     <div class="thumb-col">
                                         <div class="single-dealership-thumb">
-                                            <img src="{{ $product->photo->getUrl() }}" alt="{{ $product->name }}">
+                                            {{ $product->getFirstMedia('photo')('responsive') }}
                                         </div>
                                     </div>
                                 @else
@@ -33,7 +33,7 @@
                                     @foreach ($product->additional_photos as $photos)
                                         <div class="thumb-col">
                                             <div class="single-dealership-thumb">
-                                                <img src="{{ $photos->getUrl() }}" alt="{{ $product->name }}">
+                                                {{ $photos('responsive') }}
                                             </div>
                                         </div>
                                     @endforeach
