af06e24941d5f647f33821ee6fbf6c44516b90d2
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 20:17:29 -0700

    updated show includes

---
+++ resources/views/site/blog/partials/related-posts.blade.php
@@ -0,0 +1,50 @@
+<div class="related-post">
+    <div class="section-title">
+        <h2 class="title">Posts You’d Might Like</h2>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="single-news mt-50">
+                <div class="news-image">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/blog/blog-1.jpg')}}" alt="">
+                    </a>
+                </div>
+                <div class="news-content">
+                    <div class="news-meta">
+                        <span class="meta-cat"><a href="#">News</a></span>
+                        <span class="meta-date"><a href="#">July 31, 2020</a></span>
+                    </div>
+                    <h3 class="news-title"><a href="#">Price list of Toyota Cars 2019, The Change of Interior & Exterior</a></h3>
+                    <ul class="news-meta-bottom">
+                        <li><a href="#"><i class="ion-chatboxes"></i> 0 Comments </a></li>
+                        <li><span><i class="ion-eye"></i> 83 Viewed</span></li>
+                        <li><a href="#"><i class="ion-android-share-alt"></i> Share</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="single-news mt-50">
+                <div class="news-image">
+                    <a href="#">
+                        <img src="{{ asset('assets/images/blog/blog-2.jpg')}}" alt="">
+                    </a>
+                </div>
+                <div class="news-content">
+                    <div class="news-meta">
+                        <span class="meta-cat"><a href="#">Inspiration</a></span>
+                        <span class="meta-date"><a href="#">July 31, 2020</a></span>
+                    </div>
+                    <h3 class="news-title"><a href="#">BMW X6, The Car Trending in 2019, Best Choice in Price Range</a></h3>
+                    <ul class="news-meta-bottom">
+                        <li><a href="#"><i class="ion-chatboxes"></i> 0 Comments </a></li>
+                        <li><span><i class="ion-eye"></i> 83 Viewed</span></li>
+                        <li><a href="#"><i class="ion-android-share-alt"></i> Share</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
