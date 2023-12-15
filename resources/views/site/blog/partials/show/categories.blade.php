4aca10ee3c54c0e1f4dbd192f91bb785cd20804d
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 18:53:44 -0700

    Update categories.blade.php

--- resources/views/site/blog/partials/show/categories.blade.php
+++ resources/views/site/blog/partials/show/categories.blade.php
@@ -19,7 +19,7 @@
 
                                 <div class="category-list">
                                     <ul class="list">
-                                        @foreach($copywright_menu as $menu)
+                                        @foreach($blog_menu as $menu)
                                         <li><a href="{{ preg_replace("/%u([0-9a-f]{3,4})/i","&#x\\1;",urldecode(strpos($menu['link'], "http") === 0 ? $menu['link'] : url('', $menu['link']))) }}">{{ $menu['label'] }}</a></li>
                                         @enforeach
 
