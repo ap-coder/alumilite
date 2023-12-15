73bbf17fd1ed8b75289d85f1d8e6303b606dd7c8
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 19:04:08 -0700

    Update index.blade.php

--- resources/views/site/blog/index.blade.php
+++ resources/views/site/blog/index.blade.php
@@ -56,7 +56,8 @@
                 <div class="col-xxl-3 col-lg-4">
                     <div class="blog-sidebar">
                         @include('site.blog.partials.search')
-                        @include('site.blog.partials.categories')
+                        @include('site.blog.partials.blog-menu')
+{{--                        @include('site.blog.partials.categories')--}}
 {{--                        @include('site.blog.partials.popular-posts')--}}
 {{--                        @include('site.blog.partials.newsletters')--}}
                     </div>
