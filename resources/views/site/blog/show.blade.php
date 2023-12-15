fdee0bd4d38ab8953d103429cc7d35c7023556ff
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-14 20:26:57 -0700

    blog menu fix

--- resources/views/site/blog/show.blade.php
+++ resources/views/site/blog/show.blade.php
@@ -77,8 +77,8 @@
                     </div>
                     <div class="col-xxl-3 col-lg-4">
                         <div class="blog-sidebar">
-                            @include('site.blog.partials.show.search')
-                            @include('site.blog.partials.show.blog-menu')
+                            @include('site.blog.partials.search')
+                            @include('site.blog.partials.blog-menu')
 {{--                            @include('site.blog.partials.show.categories')--}}
 {{--                            @include('site.blog.partials.show.popular-posts')--}}
 {{--                            @include('site.blog.partials.show.newsletter')--}}
