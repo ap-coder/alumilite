fe20542cd73273e32be6880799577033675fe8a2
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2023-12-15 23:25:25 -0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -87,7 +87,8 @@ class Post extends Model implements HasMedia
         $this->addMediaConversion('original')->format(Manipulations::FORMAT_WEBP)->nonQueued();
         $this->addMediaConversion('thumb')->format(Manipulations::FORMAT_WEBP)->width(50)->height(50)->nonQueued();
         $this->addMediaConversion('preview')->format(Manipulations::FORMAT_WEBP)->width(120)->height(120)->nonQueued();
-        $this->addMediaConversion('excerpt')->format(Manipulations::FORMAT_WEBP)->width(370)->height(230)->withResponsiveImages()->nonQueued();
+        // $this->addMediaConversion('excerpt')->format(Manipulations::FORMAT_WEBP)->width(370)->height(230)->withResponsiveImages()->nonQueued();
+        $this->addMediaConversion('excerpt')->crop('crop-center',370, 230)->format(Manipulations::FORMAT_WEBP)->withResponsiveImages()->nonQueued();
         $this->addMediaConversion('responsive')->format(Manipulations::FORMAT_WEBP)->width(1200)->height(500)->withResponsiveImages()->nonQueued();
     }
 
