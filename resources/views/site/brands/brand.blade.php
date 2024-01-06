0a8a6f4e22e16056e99b22e2794a7453eab0425a
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2024-01-06 02:05:52 -0700

    added basic blocks to add to middle of page
    still need to wrap in theme template and loop each box for brands
    each box should have the image and title under it centered

--- resources/views/site/brands/brand.blade.php
+++ resources/views/site/brands/brand.blade.php
@@ -2,68 +2,168 @@
 
 @section('content')
 
-    <div style="padding-bottom:14px;padding-top:14px;text-align:center;justify-content:center;display:flex;flex-flow:row wrap;align-items:stretch;margin:0px 251.5px;max-width:1400px;width: 100%;box-sizing:border-box;">
-        <div style="text-align:centerwidth: 100% !important;padding-top:10.5px;padding-bottom:10.5px;padding-left:10.5px;padding-right:10.5px;position:relative;display:block;width: 5%;vertical-align:top;box-sizing:border-box;">
-            <div style="margin: 0px auto;max-width:900px;width: 100%;box-sizing:border-box;">
-                <h2 style="color:;font-size:30px;line-height:30px;margin-top:0px;margin:0px 0px 14px;font-weight:700;padding:0px;font-family:Saira, 'Helvetica Neue', Arial, Helvetica, sans-serif;box-sizing:border-box;">TIRE TIME</h2>
-                <p style="color:;font-size:16px;line-height:23.8px;margin:0px 0px 16px;box-sizing:border-box;">Need New Tires? No Matter What You Ride, We Have You Covered.</p>
-            </div>
-        </div> <a href="/dirtbike/tires" data-di-id="di-id-c186fb31-7886d7af" style="width: 25% !important;text-align:left;padding-top:10.5px;padding-bottom:10.5px;padding-left:10.5px;padding-right:10.5px;position:relative;display:block;vertical-align:top;color:rgba(0, 0, 0, 0.87);text-decoration:none solid rgba(0, 0, 0, 0.87);outline:rgba(0, 0, 0, 0.87) none 0px;box-sizing:border-box;background-color:rgba(0, 0, 0, 0);">
-            <div data-logging-info="{&quot;creative&quot;:&quot;home&quot;,&quot;position&quot;:12,&quot;promoType&quot;:&quot;grid&quot;,&quot;pagename&quot;:&quot;Home&quot;,&quot;name&quot;:&quot;SHOP DIRT BIKE TIRES&quot;}" style="width: 100%;position:relative;line-height:0px;box-sizing:border-box;">
-                <div style="box-sizing:border-box;">
-                    <picture style="display:block;overflow:hidden;position:relative;width: 100%;margin: 0px auto;text-align:center;box-sizing:border-box;">
-                        <source media="(max-width: 430px)" data-srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-dirt_mobile.jpg" srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-dirt_mobile.jpg" style="box-sizing:border-box;" /> <img data-src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-dirt_desktop.jpg" src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-dirt_desktop.jpg" style="height:350px;position:relative;left:164.5px;margin-left:-329px;box-sizing:border-box;border-style:none;" />
-                    </picture>
-                    <div style="height:350px;width: 100%;position:absolute;top:0px;left:0px;box-sizing:border-box;">
-                        <div style="max-width:1400px;width: 100%;position:absolute;left:164.5px;top:175px;transform:matrix(1, 0, 0, 1, -164.5, 0);padding:0px 49px;color:rgb(255, 255, 255);box-sizing:border-box;">
-                            <div style="padding:0px;overflow-wrap:break-word;text-align:center;margin: 0px auto;max-width:850px;box-sizing:border-box;"> </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style="text-align:centerfont-size:16px;line-height:23.8px;box-sizing:border-box;">
-                <h5 style="font-size:16px;margin-top:14px;line-height:20.5712px;margin:14px 0px;font-weight:700;padding:0px;font-family:Poppins, 'Helvetica Neue', Arial, Helvetica, sans-serif;box-sizing:border-box;"><span style="color:; border-color:border-color:rgba(0, 0, 0, 0.87);border-bottom:1px solid rgba(0, 0, 0, 0.87);box-sizing:border-box;">SHOP DIRT BIKE TIRES</span></h5>
-                <p style="color:;margin:0px 0px 16px;line-height:23.8px;box-sizing:border-box;"></p>
-            </div>
-        </a> <a href="/streetbike/tires" data-di-id="di-id-4a1149ca-cac99ffa" style="width: 25% !important;text-align:left;padding-top:10.5px;padding-bottom:10.5px;padding-left:10.5px;padding-right:10.5px;position:relative;display:block;vertical-align:top;color:rgba(0, 0, 0, 0.87);text-decoration:none solid rgba(0, 0, 0, 0.87);outline:rgba(0, 0, 0, 0.87) none 0px;box-sizing:border-box;background-color:rgba(0, 0, 0, 0);">
-            <div data-logging-info="{&quot;creative&quot;:&quot;home&quot;,&quot;position&quot;:13,&quot;promoType&quot;:&quot;grid&quot;,&quot;pagename&quot;:&quot;Home&quot;,&quot;name&quot;:&quot;SHOP STREET BIKE TIRES&quot;}" style="width: 100%;position:relative;line-height:0px;box-sizing:border-box;">
-                <div style="box-sizing:border-box;">
-                    <picture style="display:block;overflow:hidden;position:relative;width: 100%;margin: 0px auto;text-align:center;box-sizing:border-box;">
-                        <source media="(max-width: 430px)" data-srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-street_mobile.jpg" srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-street_mobile.jpg" style="box-sizing:border-box;" /> <img data-src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-street_desktop.jpg" src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-street_desktop.jpg" style="height:350px;position:relative;left:164.5px;margin-left:-329px;box-sizing:border-box;border-style:none;" />
-                    </picture>
-                    <div style="height:350px;width: 100%;position:absolute;top:0px;left:0px;box-sizing:border-box;">
-                        <div style="max-width:1400px;width: 100%;position:absolute;left:164.5px;top:175px;transform:matrix(1, 0, 0, 1, -164.5, 0);padding:0px 49px;color:rgb(255, 255, 255);box-sizing:border-box;">
-                            <div style="padding:0px;overflow-wrap:break-word;text-align:center;margin: 0px auto;max-width:850px;box-sizing:border-box;"> </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style="text-align:centerfont-size:16px;line-height:23.8px;box-sizing:border-box;">
-                <h5 style="font-size:16px;margin-top:14px;line-height:20.5712px;margin:14px 0px;font-weight:700;padding:0px;font-family:Poppins, 'Helvetica Neue', Arial, Helvetica, sans-serif;box-sizing:border-box;"><span style="color:; border-color:border-color:rgba(0, 0, 0, 0.87);border-bottom:1px solid rgba(0, 0, 0, 0.87);box-sizing:border-box;">SHOP STREET BIKE TIRES</span></h5>
-                <p style="color:;margin:0px 0px 16px;line-height:23.8px;box-sizing:border-box;"></p>
-            </div>
-        </a> <a href="/atv/tires" data-di-id="di-id-3b542b15-e04b4492" style="width: 25% !important;text-align:left;padding-top:10.5px;padding-bottom:10.5px;padding-left:10.5px;padding-right:10.5px;position:relative;display:block;vertical-align:top;color:rgba(0, 0, 0, 0.87);text-decoration:none solid rgba(0, 0, 0, 0.87);outline:rgba(0, 0, 0, 0.87) none 0px;box-sizing:border-box;background-color:rgba(0, 0, 0, 0);">
-            <div data-logging-info="{&quot;creative&quot;:&quot;home&quot;,&quot;position&quot;:14,&quot;promoType&quot;:&quot;grid&quot;,&quot;pagename&quot;:&quot;Home&quot;,&quot;name&quot;:&quot;SHOP ATV TIRES&quot;}" style="width: 100%;position:relative;line-height:0px;box-sizing:border-box;">
-                <div style="box-sizing:border-box;">
-                    <picture style="display:block;overflow:hidden;position:relative;width: 100%;margin: 0px auto;text-align:center;box-sizing:border-box;">
-                        <source media="(max-width: 430px)" data-srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-atv_mobile.jpg" srcset="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-atv_mobile.jpg" style="box-sizing:border-box;" /> <img data-src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-atv_desktop.jpg" src="https://f-static.motosport.com/motographics/images/home/all_storefront/2023/230123_Homepage/230123-tires-atv_desktop.jpg" style="height:350px;position:relative;left:164.5px;margin-left:-329px;box-sizing:border-box;border-style:none;" />
-                    </picture>
-                    <div style="height:350px;width: 100%;position:absolute;top:0px;left:0px;box-sizing:border-box;">
-                        <div style="max-width:1400px;width: 100%;position:absolute;left:164.5px;top:175px;transform:matrix(1, 0, 0, 1, -164.5, 0);padding:0px 49px;color:rgb(255, 255, 255);box-sizing:border-box;">
-                            <div style="padding:0px;overflow-wrap:break-word;text-align:center;margin: 0px auto;max-width:850px;box-sizing:border-box;"> </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div style="text-align:centerfont-size:16px;line-height:23.8px;box-sizing:border-box;">
-                <h5 style="font-size:16px;margin-top:14px;line-height:20.5712px;margin:14px 0px;font-weight:700;padding:0px;font-family:Poppins, 'Helvetica Neue', Arial, Helvetica, sans-serif;box-sizing:border-box;"><span style="color:; border-color:border-color:rgba(0, 0, 0, 0.87);border-bottom:1px solid rgba(0, 0, 0, 0.87);box-sizing:border-box;">SHOP ATV TIRES</span></h5>
-                <p style="color:;margin:0px 0px 16px;line-height:23.8px;box-sizing:border-box;"></p>
-            </div>
-        </a>
+    <div class="gallery">
+        <div class="gallery__column">
+            <a href="https://unsplash.com/@jeka_fe" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/_cvwXhGqG-o/300x300" alt="Portrait by Jessica Felicio" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Jessica Felicio</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@oladimeg" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/AHBvAIVqk64/300x500" alt="Portrait by Oladimeji Odunsi" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Oladimeji Odunsi</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@a2eorigins" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/VLPLo-GtrIE/300x300" alt="Portrait by Alex Perez" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Alex Perez</figcaption>
+                </figure>
+            </a>
+        </div>
+
+        <div class="gallery__column">
+            <a href="https://unsplash.com/@hikiapp" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/A9rQeI2AdR4/300x300" alt="Portrait by Hikiapp" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Hikiapp</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@von_co" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/dnL6ZIpht2s/300x300" alt="Portrait by Ivana Cajina" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Ivana Cajina</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@j_erhunse" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/vp9mRauo68c/300x500" alt="Portrait by Jeffery Erhunse" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Jeffery Erhunse</figcaption>
+                </figure>
+            </a>
+        </div>
+
+        <div class="gallery__column">
+            <a href="https://unsplash.com/@marilezhava" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/Xm9-vA_bhm0/300x500" alt="Portrait by Mari Lezhava" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Mari Lezhava</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@ethanhaddox" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/NTjSR3zYpsY/300x300" alt="Portrait by Ethan Haddox" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Ethan Haddox</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@mr_geshani" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/2JH8d3ChNec/300x300" alt="Portrait by Amir Geshani" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Amir Geshani</figcaption>
+                </figure>
+            </a>
+        </div>
+
+        <div class="gallery__column">
+            <a href="https://unsplash.com/@nixcreative" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/sh3LSNbyj7k/300x300" alt="Portrait by Tyler Nix" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Tyler Nix</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@majestical_jasmin" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/OQd9zONSx7s/300x300" alt="Portrait by Jasmin Chew" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Jasmin Chew</figcaption>
+                </figure>
+            </a>
+
+            <a href="https://unsplash.com/@dimadallacqua" target="_blank" class="gallery__link">
+                <figure class="gallery__thumb">
+                    <img src="https://source.unsplash.com/XZkEhowjx8k/300x500" alt="Portrait by Dima DallAcqua" class="gallery__image">
+                    <figcaption class="gallery__caption">Portrait by Dima DallAcqua</figcaption>
+                </figure>
+            </a>
+        </div>
     </div>
+
+
+
 
 @endsection
 
-@section('headcss') @endsection
+@section('headcss')
+    <style>
+        .gallery {
+            display: flex;
+            padding: 2px;
+            transition: .3s;
+
+        &:hover &__image {
+                     filter: grayscale(1);
+                 }
+
+        &__column {
+             display: flex;
+             flex-direction: column;
+             width: 25%;
+         }
+
+        &__link {
+             margin: 2px;
+             overflow: hidden;
+
+        &:hover {
+        .gallery__image {
+            filter: grayscale(0);
+        }
+
+        .gallery__caption {
+            opacity: 1;
+        }
+        }
+        }
+
+        &__thumb {
+             position: relative;
+         }
+
+        &__image {
+             display: block;
+             width: 100%;
+             transition: .3s;
+
+        &:hover {
+             transform: scale(1.1);
+         }
+        }
+
+        &__caption {
+             position: absolute;
+             bottom: 0;
+             left: 0;
+             padding: 25px 15px 15px;
+             width: 100%;
+             font-family: 'Raleway', sans-serif;
+             font-size: 16px;
+             color: white;
+             opacity: 0;
+             background: linear-gradient(0deg, rgba(0, 0, 0, .5) 0%, rgba(255, 255 ,255 , 0) 100%);
+             transition: .3s;
+         }
+        }
+
+    </style>
+@endsection
+
 @section('headjs') @endsection
 @section('footjs') @endsection
