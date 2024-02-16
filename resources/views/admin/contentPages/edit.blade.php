195d092ab80bdeaf31a76c1e970135731dd07bab
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2024-02-16 09:30:53 -0700

    Update edit.blade.php

--- resources/views/admin/contentPages/edit.blade.php
+++ resources/views/admin/contentPages/edit.blade.php
@@ -2,30 +2,10 @@
 @section('content')
 
     <style>
-        .media-images .removeImage {
-            position: absolute;
-            right: 10px;
-            color: red;
-        }
-
-        body.loading .overlaymodal {
-            overflow: hidden;
-        }
-
-
-        body.loading .overlaymodal {
-            display: block;
-        }
-        .overlaymodal {
-            display: none;
-            position: fixed;
-            z-index: 1000;
-            top: 0;
-            left: 0;
-            height: 100%;
-            width: 100%;
-            background: rgba( 255, 255, 255, .8) url('http://i.stack.imgur.com/FhHRx.gif') 50% 50% no-repeat;
-        }
+        .media-images .removeImage {position: absolute; right: 10px; color: red; }
+        body.loading .overlaymodal {overflow: hidden; }
+        body.loading .overlaymodal {display: block; }
+        .overlaymodal {display: none; position: fixed; z-index: 1000; top: 0; left: 0; height: 100%; width: 100%; background: rgba( 255, 255, 255, .8) url('http://i.stack.imgur.com/FhHRx.gif') 50% 50% no-repeat; }
     </style>
 
 
