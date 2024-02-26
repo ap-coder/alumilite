eb9d6655165b34c18a1c5a964c30ef9eb38afcbf
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2024-02-26 15:34:07 -0700

    more updates

--- resources/views/site/products/show.blade.php
+++ resources/views/site/products/show.blade.php
@@ -14,43 +14,7 @@
             <li class="breadcrumb-item active">{{ $product->name }}</li>
         </ul>
         <div class="listing-social d-lg-flex justify-content-between">
-            <div class="listing-btn">
-
-            </div>
-            <div class="social-share">
-                <ul class="social">
-                    <li>
-                        <a class="facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ route('products.show', $product->slug ) }}" target="_blank">
-                            <i class="ion-social-facebook"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="twitter" href="https://twitter.com/intent/tweet?text={{ $product->name }}&url={{ route('products.show', $product->slug ) }}" target="_blank">
-                            <i class="ion-social-twitter"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="pinterest" href="https://pinterest.com/pin/create/button/?url={{ route('products.show', $product->slug ) }}&media={{ $product->photo ? $product->photo->getUrl() : '' }}&description={{ $product->name }}" target="_blank">
-                            <i class="ion-social-pinterest-outline"></i>
-                        </a>
-                        </li>
-                    <li>
-                        <a class="tumblr" href="https://www.tumblr.com/share/link?url={{ route('products.show', $product->slug ) }}&name={{ $product->name }}&description={{ $product->excerpt }}"  target="_blank">
-                            <i class="ion-social-tumblr"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="reddit" href="https://www.reddit.com/submit?url={{ route('products.show', $product->slug ) }}&title={{ $product->name }}" target="_blank">
-                            <i class="ion-social-reddit"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="linkedin" href="https://www.linkedin.com/shareArticle?mini=true&url={{ route('products.show', $product->slug ) }}&title={{ $product->name }}" target="_blank">
-                            <i class="ion-social-linkedin"></i>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            @include('site.products.show.partials.s')
         </div>
         <div class="title-price">
             <div class="title-excerpt">
