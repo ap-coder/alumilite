5d66962c906b76734d182d1ee157eb9e6de56e08
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2024-02-26 16:11:25 -0700

    Update show.blade.php

--- resources/views/site/products/show.blade.php
+++ resources/views/site/products/show.blade.php
@@ -8,18 +8,18 @@
         <ul class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ url('/') }}">Home</a></li>
             @if ($product->categories->count() > 0)
-                <li class="breadcrumb-item"><a
-                        href="javascript:void(0);">{{ $product->categories->first()->name }}</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0);">{{ $product->categories->first()->name }}</a></li>
             @endif
             <li class="breadcrumb-item active">{{ $product->name }}</li>
         </ul>
-        <div class="listing-social d-lg-flex justify-content-between">
-            @include('site.products.show.partials.social-share')
-        </div>
+
         <div class="title-price">
             <div class="title-excerpt">
                 <h3 class="entry-title">{{ $product->name }} <i class="ion-android-checkmark-circle"></i></h3>
-                <p class="entry-excerpt"> @if($product->brand) <a href="{{ route('brands.show',$product->brand->slug) }}">{{ $product->brand->name }}</a> @endif {{ $product->brand_model ? $product->brand_model->model : '' }}</p>
+                <p class="entry-excerpt"> @if($product->brand)
+                        <a href="{{ route('brands.show',$product->brand->slug) }}">{{ $product->brand->name }}</a> @endif
+                    {{ $product->brand_model ? '| Model: ' . $product->brand_model->model : '' }}
+                </p>
             </div>
             <div class="price">
                 <span class="price">
@@ -59,6 +59,9 @@
                             'products' => $similarProducts,
                         ])
 
+                        <div class="listing-social d-lg-flex justify-content-between">
+                            @include('site.products.show.partials.social-share')
+                        </div>
                     </div>
                     @include('site.products.show.partials.sidebar')
 
