bffc3a3ab85c5971276b1a64478a28d1980b628a
PhillipMRE <135060060+PhillipMRE@users.noreply.github.com>
2024-02-27 14:55:57 -0700

    Create 2024_02_27_201046_add_2_fields_to_slider.php

---
+++ database/migrations/2024_02_27_201046_add_2_fields_to_slider.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sliders', function (Blueprint $table) {
+            $table->string('main_button_icon_class')->nullable();
+            $table->string('second_button_icon_class')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sliders', function (Blueprint $table) {
+            //
+        });
+    }
+};
